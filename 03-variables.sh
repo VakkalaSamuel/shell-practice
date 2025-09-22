2697671c8d13fb8a6288529d7a09b797312dfaa7
samuel vakkala <vakkalasamuelsam123@gmail.com>
2025-09-22 17:50:28 +0530

    some message

---
+++ 03-variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+# No space before or after equal
+PERSON1=John
+PERSON2=Raheem
+
+echo "$1:: Hi Suresh, How are you?"
+echo "$2:: Hello Ramesh, I am fine. How are you?"
+echo "$1:: What are you learning recently?"
+echo "$2:: I am learning Shell scripting Ramesh"
