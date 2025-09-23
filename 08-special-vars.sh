9871e7e9ff7f7fa611ce498f889ddd8d208463ae
samuel vakkala <vakkalasamuelsam123@gmail.com>
2025-09-23 12:52:09 +0530

    some message

--- 08-special-vars.sh
+++ 08-special-vars.sh
@@ -3,3 +3,6 @@
 echo "ALL variables passed to the script: $@"
 echo "All variables passed to the script : $*"
 echo "Script name: $0"
+echo "Current directory: $PWD"
+echo "Who is running this: $USER"
+echo "Home directory of user: $HOME
