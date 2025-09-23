e62214f35233a17595c985eff5b286aeae7b31d4
samuel vakkala <vakkalasamuelsam123@gmail.com>
2025-09-23 15:12:15 +0530

    some message

---
+++ 14-installation.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]; then
+    echo "ERROR:: Please run this script with root privelege"
+    exit 1 # failure is other than 0
+fi
+
+dnf install mysql -y
+
+if [ $? -ne 0 ]; then
+    echo "ERROR:: Installing MySQL is failure"
+    exit 1
+else
+    echo "Installing MySQL is SUCCESS"
+fi
+
+dnf install nginx -y
+
+if [ $? -ne 0 ]; then
+    echo "ERROR:: Installing nginx is failure"
+    exit 1
+else
+    echo "Installing nginx is SUCCESS"
+fi
+
+dnf install mongodb-mongosh -y
+
+if [ $? -ne 0 ]; then
+    echo "ERROR:: Installing mongodb-mongosh is failure"
+    exit 1
+else
+    echo "Installing mongodb-mongosh is SUCCESS"
+fi
