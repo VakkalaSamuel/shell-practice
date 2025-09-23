6ad23d37a0bc9bd4912d187431ca2f734bf98346
samuel vakkala <vakkalasamuelsam123@gmail.com>
2025-09-23 15:24:17 +0530

    some message

--- 14-installation.sh
+++ 14-installation.sh
@@ -7,21 +7,30 @@ if [ $USERID -ne 0 ]; then
     exit 1 # failure is other than 0
 fi
 
-VALIDATE(){ #functions receive inputs through args just like shell script args
-    if [ $1 -ne 0 ]; then
-        echo "ERROR:: Installing $2 is failure"
-        exit 1
-else
-    echo "Installing $2 is SUCCESS"
-fi
-}
-
 
 dnf install mysql -y
-VALIDATE $? "MySQL"
+
+if [ $? -ne 0 ]; then
+    echo "ERROR:: Installing MySQL is failure"
+    exit 1
+else
+    echo "Installing MySQL is SUCCESS"
+fi
 
 dnf install nginx -y
-VALIDATE $? "Nginx"
 
-dnf install python3 -y
-VALIDATE $? "python3"
+if [ $? -ne 0 ]; then
+    echo "ERROR:: Installing nginx is failure"
+    exit 1
+else
+    echo "Installing nginx is SUCCESS"
+fi
+
+dnf install phython3 -y
+
+if [ $? -ne 0 ]; then
+    echo "ERROR:: Installing mongodb-mongosh is failure"
+    exit 1
+else
+    echo "Installing mongodb-mongosh is SUCCESS"
+fi
